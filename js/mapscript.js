68df72a46389792dd8f5af4cacee814544aab243
EliaShaj <elias.hajjar@metropolia.fi>
2019-12-05 03:48:27 +0200

    V0.02

---
+++ js/mapscript.js
@@ -0,0 +1,13 @@
+'use strict';
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([24.945831, 60.192059]),
+        zoom: 10
+    })
+});
