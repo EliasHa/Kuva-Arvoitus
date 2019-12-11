a166c39afe5a97b047c65911a226f97fc2a307b7
EliaShaj <elias.hajjar@metropolia.fi>
2019-12-11 17:07:50 +0200

    V0.04: Some basework for backend login function.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,15 @@
 'use strict';
-const {validationResult} = require('express-validator');
+
 const userModel = require('../models/userModel');
 
 const user_create_post = async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        console.log('user create error', errors);
-        res.send(errors.array());
-    } else {
-        const user = [
-            req.body.name,
-            req.body.email,
-            req.body.passwd,
-        ];
-        const result = await userModel.addUser(user);
-        await res.json(result);
-    }
+    const user = [
+        req.body.email,
+        req.body.name,
+        req.body.passwd,
+    ];
+    const result = await userModel.addUser(user);
+    await res.json(result);
 };
 
 module.exports = {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,16 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
-const {body, sanitizeBody} = require('express-validator');
 const userController = require('../controllers/userController');
 
-router.post('/', [
-    body('name', 'minimum 3 characters').isLength({min: 3}),
-    body('email', 'email is not valid').isEmail(),
-    body('passwd', 'at least one upper case letter').
-    matches('(?=.*[A-Z]).{8,}'),
-    sanitizeBody('name').escape(),
-], userController.user_create_post);
+router.post('/', userController.user_create_post);
 
 
 module.exports = router;
