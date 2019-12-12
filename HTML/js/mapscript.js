58d5adce39a355637351fb63530d3ca69f4b0483
EliaShaj <elias.hajjar@metropolia.fi>
2019-12-13 01:10:33 +0200

    V0.7.3: Added timer for map guessing.

--- HTML/js/mapscript.js
+++ HTML/js/mapscript.js
@@ -2,6 +2,18 @@
 window.onload = function () {
     var marker;
     var guessButton = document.getElementById('guess-button');
+    var guessStartTime = new Date().getTime();
+    var timeDifference;
+
+    var timer = setInterval(function () {
+        var now = new Date().getTime();
+        timeDifference = now - guessStartTime;
+        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        document.getElementById('guess-time').innerHTML = minutes + 'm ' + seconds + 's ';
+
+
+    });
 
     mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpYXNoYWoiLCJhIjoiY2szeW9rdjRlMG5zMTNtcWM5aHFvcjJiMSJ9.0KHgS7rc1xWuAGzD1HpFiQ';
     var map = new mapboxgl.Map({
@@ -43,5 +55,7 @@ window.onload = function () {
 
     function printGuess() {
         console.log(JSON.stringify(marker.getLngLat()));
+        console.log('guess time: ' + timeDifference);
+        console.log('guess time: ' + Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)) + 'm ' + Math.floor((timeDifference % (1000 * 60)) / 1000) + 's ');
     }
 };
