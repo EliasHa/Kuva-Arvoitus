dd13603ce9fe762096bda2c81e434516b3d2329c
EliaShaj <elias.hajjar@metropolia.fi>
2019-12-12 10:22:13 +0200

    V0.7.1: Added map marker functionality and guess button (functionality of the button is still not done)

--- HTML/js/mapscript.js
+++ HTML/js/mapscript.js
@@ -1,10 +1,32 @@
 'use strict';
-mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpYXNoYWoiLCJhIjoiY2szeW9rdjRlMG5zMTNtcWM5aHFvcjJiMSJ9.0KHgS7rc1xWuAGzD1HpFiQ';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [24.96, 60.19],
-    zoom: 7
-});
+window.onload = function () {
+    var marker;
+    var guessButton = document.getElementById('guess-button');
 
-map.addControl(new mapboxgl.NavigationControl());
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpYXNoYWoiLCJhIjoiY2szeW9rdjRlMG5zMTNtcWM5aHFvcjJiMSJ9.0KHgS7rc1xWuAGzD1HpFiQ';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [24.96, 60.19],
+        zoom: 7
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+
+    map.on('click', function (e) {
+        console.log('mousedown');
+        if (marker === undefined){
+            guessButton.style.cursor = 'pointer';
+            guessButton.style.backgroundColor = '#add8e6';
+            console.log('marker undefined');
+            marker = new mapboxgl.Marker({
+                draggable: true
+            })
+                .setLngLat(e.lngLat).addTo(map);
+        } else {
+            marker.setLngLat(e.lngLat);
+        }
+
+    });
+};
